aa2d120204347a101b9691672334c4f5cff6340e
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-05-06 17:39:44 -0500

    limpiando app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from "./platzi.webp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <TodoItem/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoItem />
     </div>
   );
 }
 
-
-
 function TodoItem() {
   return (
     <li>
-      <span>v</span> 
+      <span>v</span>
       <p>Llorar con la llorona</p>
       <span>X </span>
     </li>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
