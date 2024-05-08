afc902627800fb120ec92add20b4802d7987cf69
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-05-08 17:16:27 -0500

    Agregando mas props a mi component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ import TodoList from "./components/TodoList/TodoList";
 import TodoItem from "./components/TodoItem/TodoItem";
 import CreateTodoButton from "./components/CreateTodoButton/CreateTodoButton";
 
+const TodoDefault = [
+  {
+    id: 1,
+    text: "tarea 1",
+    completed: false,
+  },
+
+  {
+    id: 2,
+    text: "tarea 2",
+    completed: false,
+  },
+
+  {
+    id: 3,
+    text: "tarea 3",
+    completed: false,
+  },
+
+  {
+    id: 4,
+    text: "tarea 4",
+    completed: false,
+  },
+];
+
 function App() {
   return (
     <div className="App">
-      <TodoCounter completed={16} total={25}/>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {TodoDefault.map((todo) => (
+          <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function TodoItem() {
+function TodoItem({text, completed}) {
   return (
     <li>
     <span>v</span>
-    <p>Llorar con la llorona</p>
+    <p>{text} </p>
+
     <span>X </span>
   </li>
   )
